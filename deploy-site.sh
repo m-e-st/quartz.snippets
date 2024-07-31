03029f5b11ee1f212aaa6b4ae1c1fc3dd26c078c
m-e-st <michael@stumpp.name>
2024-07-31 11:44:59 +0200

    Quartz sync: Jul 31, 2024, 11:44 AM

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,4 +1,6 @@
 npx quartz sync
 npx quartz build --output /home/micha/NAS/Web/infothek --baseDir 172.16.1.4/infothek/
 cp .htaccess /home/micha/NAS/Web/infothek/
+mkdir -p /home/micha/NAS/Web/static
+cp quartz/static/quartzLogo /home/micha/NAS/Web/static
 
